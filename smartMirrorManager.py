4943c2e97f7fc00feb7ad8c6233b7aa107180125
Diego Martinez <diegomtzg96@gmail.com>
2018-01-16 19:26:16 -0500

    implemented signal filter to have 'q' exit app

--- smartMirrorManager.py
+++ smartMirrorManager.py
@@ -13,21 +13,20 @@ small_fontsize = 12
 med_fontsize = 18
 large_fontsize = 28
 xlarge_fontsize = 48
-
-
-def main():
-    smartMirror_app = QApplication(sys.argv) # Create application (runnable from command line)
-    window = mainUI() # Create application window
-    sys.exit(smartMirror_app.exec_()) # Ensure clean app exit
-
+global smartMirrorApp
 
 class mainUI:
     def __init__(self):
         self.qt = QWidget()
         self.initUI()
 
-
     def initUI(self):
+        self.qt.showFullScreen()
+
+        #Install signal filter to receive 'q' clicks to be able to quit app
+        filter = QKeyFilter(self.qt)
+        self.qt.installEventFilter(filter)
+
         # Make background dark
         self.darkPalette = QPalette()
         # doesn't work self.darkPalette.setColor(QPalette.Foreground, Qt.white)
@@ -47,8 +46,19 @@ class mainUI:
         self.qt.hbox1.addWidget(self.qt.clock)
 
         self.qt.setLayout(self.qt.hbox1)
-        self.qt.showFullScreen()
+
+# idk how this all really works but hey at least we can quit the app by clicking 'q'
+class QKeyFilter(QObject):
+    qKeyPressed = pyqtSignal()
+
+    def eventFilter(self, obj, event):
+        if event.type() == QEvent.KeyPress:
+            if event.key() == Qt.Key_Q:
+                sys.exit(smartMirrorApp.exec_())
+        return False
 
 
 if __name__ == '__main__':
-    main()
+    smartMirrorApp = QApplication(sys.argv)  # Create application (runnable from command line)
+    window = mainUI()  # Create application window
+    sys.exit(smartMirrorApp.exec_())  # Ensure clean app exit
