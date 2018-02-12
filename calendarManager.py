aadac140f2573d904c4a15bfce09cb4fc47782ff
Diego Martinez <diegomtzg96@gmail.com>
2018-02-11 23:40:25 -0500

    calendar bugfix

--- calendarManager.py
+++ calendarManager.py
@@ -108,7 +108,7 @@ class Calendar(QWidget):
         credential_dir = os.path.join(home_dir, '.credentials')
         if not os.path.exists(credential_dir):
             os.makedirs(credential_dir)
-        credential_path = os.path.join(credential_dir,'calendar-python-quickstart.json')
+        credential_path = os.path.join(credential_dir, CLIENT_SECRET_FILE)
         store = Storage(credential_path)
         credentials = store.get()
         if not credentials or credentials.invalid:
